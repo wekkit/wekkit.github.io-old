515c95a445fd4f3615db21e009e2597420b1a2a4
Keith Chia <email@keithchia.com>
2017-03-13 18:48:22 +0800

    optimized hero image

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ class Nav extends Component {
       <div className='container'>
       <div className='row row-nav'>
           <Link activeClass="active" className='btn btn-primary' to="portfolio" smooth={true} offset={0} duration={500} >Portfolio</Link>
-          <Link activeClass="active" className='btn btn-primary' to="resume" smooth={true} offset={75} duration={500} >Resume</Link>
+          <Link activeClass="active" className='btn btn-primary' to="resume" smooth={true} offset={75} duration={500} >Skills</Link>
           <Link activeClass="active" className='btn btn-primary' to="contact" smooth={true} offset={75} duration={500} >Contact</Link>
       </div>
       </div>
